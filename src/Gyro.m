7bc155802a7c056cdc4dd7a9e35f0c8c596b9efd
fadeto404 <rskjel18@student.aau.dk>
2022-11-30 12:32:57 +0100

    Slightly improved computational performance

--- src/Gyro.m
+++ src/Gyro.m
@@ -9,6 +9,7 @@ classdef Gyro
         disc_noise {mustBeNumeric} % Discretized noise variance
         disc_bias {mustBeNumeric} % Discretized bias variance
         S {mustBeNumeric} % Scale factor and misalignment matrix
+        IS {mustBeNumeric} % I_3 + S
     end
     methods
         function obj = Gyro(noise_var, bias_var, b0, dt, S)
@@ -18,6 +19,7 @@ classdef Gyro
             obj.bias = b0;
             obj.dt = dt;
             obj.S = S;
+            obj.IS = eye(3) + obj.S;
             obj.bias_var = bias_var(1);
             obj.noise_var = noise_var(1);
             obj.disc_noise = sqrt(obj.noise_var/obj.dt + obj.bias_var*obj.dt/12);
@@ -30,7 +32,7 @@ classdef Gyro
             bias = obj.bias;
             % w_out = (I+S)*w_true + eta_v + eta_u, 
             % eta_v ~ N(0,R_v), eta_u(t) ~ N(µ_u,R_u), µ_u = b0
-            omega_meas = (eye(3) + obj.S)*omega_true + ...
+            omega_meas = obj.IS*omega_true + ...
                          obj.disc_noise*randn(3,1) + ...
                          0.5*(bias_prev + bias);
         end

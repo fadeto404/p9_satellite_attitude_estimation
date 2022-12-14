23b0b831dc67f1acf8d8b5f9db2f56ca97675fce
fadeto404 <rskjel18@student.aau.dk>
2022-12-14 14:12:37 +0100

    Minor change

--- src/mag_calibration_ekf_showcase.m
+++ src/mag_calibration_ekf_showcase.m
@@ -227,34 +227,34 @@ for k = 1:300:N
 %     end
 end
 
-%% Animation
-x_initial = orbit_state0(1);
-y_initial = orbit_state0(2);
-z_initial = orbit_state0(3);
-a = scatter3(x_initial, y_initial, z_initial,'MarkerEdgeColor','r','LineWidth',1,Marker="o");
-[lat,long,rad] = cart2sph(x_initial,z_initial,y_initial);
-[XYZ,H,D,I,F,DXDYDZ,DH,DD,DI,DF] = igrfmagm(rad,lat,long,decyear(datetime('today')));
-MagNorm = XYZ/norm(XYZ);
-b = plot3([0,MagNorm(1)*1e6],[0,MagNorm(2)*1e6],[0,MagNorm(3)*1e6],'color','k','linewidth',2);
-for i = T
-    x = nanoTesla*cos(i);
-    y = nanoTesla*sin(i)*cos(deg2rad(theta));
-    z = -nanoTesla*sin(i)*sin(deg2rad(theta)); 
-    drawnow;
-    rho = norm([x y z]);
-    thetaE = acos(z/rho);
-    phiE = 0;
-    psiE = atan2(y,x);
-    lat = 90-thetaE*180/pi;
-    long = psiE*180/pi;
-    alt = (rho - EarthRad);
-%     [lat,long,rad] = cart2sph(x,y,z);
-    [XYZ,H,D,I,F,DXDYDZ,DH,DD,DI,DF] = igrfmagm(alt,lat,long,decyear(datetime('today')));
-    BNED = [XYZ(1);XYZ(2);-XYZ(3)];
-    BI = TIB(phiE,thetaE+pi,psiE)*BNED;
-    MagNorm = BI/norm(BI);
-    delete(a);
-    delete(b);
-    a = scatter3(x, y, z,'MarkerEdgeColor','r','LineWidth',1,Marker="o");
-    b = plot3([x,x+MagNorm(1)*1e6],[y,y+MagNorm(2)*1e6],[z,z+MagNorm(3)*1e6],'color','k','linewidth',2);
-end
+%% Animation (from Sid, kills laptops)
+% x_initial = orbit_state0(1);
+% y_initial = orbit_state0(2);
+% z_initial = orbit_state0(3);
+% a = scatter3(x_initial, y_initial, z_initial,'MarkerEdgeColor','r','LineWidth',1,Marker="o");
+% [lat,long,rad] = cart2sph(x_initial,z_initial,y_initial);
+% [XYZ,H,D,I,F,DXDYDZ,DH,DD,DI,DF] = igrfmagm(rad,lat,long,decyear(datetime('today')));
+% MagNorm = XYZ/norm(XYZ);
+% b = plot3([0,MagNorm(1)*1e6],[0,MagNorm(2)*1e6],[0,MagNorm(3)*1e6],'color','k','linewidth',2);
+% for i = T
+%     x = nanoTesla*cos(i);
+%     y = nanoTesla*sin(i)*cos(deg2rad(theta));
+%     z = -nanoTesla*sin(i)*sin(deg2rad(theta)); 
+%     drawnow;
+%     rho = norm([x y z]);
+%     thetaE = acos(z/rho);
+%     phiE = 0;
+%     psiE = atan2(y,x);
+%     lat = 90-thetaE*180/pi;
+%     long = psiE*180/pi;
+%     alt = (rho - EarthRad);
+% %     [lat,long,rad] = cart2sph(x,y,z);
+%     [XYZ,H,D,I,F,DXDYDZ,DH,DD,DI,DF] = igrfmagm(alt,lat,long,decyear(datetime('today')));
+%     BNED = [XYZ(1);XYZ(2);-XYZ(3)];
+%     BI = TIB(phiE,thetaE+pi,psiE)*BNED;
+%     MagNorm = BI/norm(BI);
+%     delete(a);
+%     delete(b);
+%     a = scatter3(x, y, z,'MarkerEdgeColor','r','LineWidth',1,Marker="o");
+%     b = plot3([x,x+MagNorm(1)*1e6],[y,y+MagNorm(2)*1e6],[z,z+MagNorm(3)*1e6],'color','k','linewidth',2);
+% end
